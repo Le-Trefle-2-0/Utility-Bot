2484cf9335d29f9b7855356fb89c0f069154f1ba
Nininanou16 <paul.peronredon@skypvp.fr>
2022-08-17 17:04:45 +0200

    add await to start check directory creation

--- Utility/StartCheck.js
+++ Utility/StartCheck.js
@@ -1,21 +1,21 @@
 const { existsSync, mkdir } = require('fs');
 
-module.exports = () => {
-    if (!existsSync('../Logs')) {
-        mkdir('../Logs', (err) => {
+module.exports = async () => {
+    if (!existsSync('./Logs')) {
+        await mkdir('./Logs', (err) => {
             if (err) throw err;
         });
     }
 
-    if (!existsSync('../Logs/Errors')) {
-        mkdir('../Logs/Errors', (err) => {
+    if (!existsSync('./Logs/Errors')) {
+        await mkdir('./Logs/Errors', (err) => {
             if (err) throw err;
         }
         );
     }
 
-    if (!existsSync('../Logs/Warnings')) {
-        mkdir('../Logs/Warnings', (err) => {
+    if (!existsSync('./Logs/Warnings')) {
+        await mkdir('./Logs/Warnings', (err) => {
             if (err) throw err;
         }
         );
