98c1100172a936f42e0cb6381010bc3f21f603f3
Nininanou16 <paul.peronredon@skypvp.fr>
2022-08-17 17:05:41 +0200

    Advanced log with UUID, log saving, and console colors

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ require('dotenv').config();
 require('./Utility/StartCheck')();
 
 const Client = require('./Bot').login();
+Client.log = require('./Utility/console');
+// Client.log.error('test error')
