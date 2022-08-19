c6d195d77fdbc7ea1a65339f1d5a8b3c6b5da6f2
Nininanou16 <paul.peronredon@skypvp.fr>
2022-08-19 12:49:57 +0200

    Finish buttons & menus handler + add examples

--- Handlers/buttons.js
+++ Handlers/buttons.js
@@ -1,3 +1,13 @@
-module.exports = (Client) => {
+const { readdir } = require('fs');
 
+module.exports = (Client) => {
+    readdir('./Buttons', (err, files) => {
+        if (err) throw err
+    
+        files.forEach(file => {
+            let name = file.split('.')[0];
+
+            Client.buttons.set(name, require(`./Buttons/${file}`));
+        })
+    });
 }
