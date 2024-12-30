316bc67c7fdacc08d50a62acb73e16e0e367054f
Nininanou16 <71654580+Nininanou16@users.noreply.github.com>
2024-12-30 11:17:18 +0100

    finalize opening and closing scheduling system

--- Events/ready.js
+++ Events/ready.js
@@ -1,42 +1,111 @@
-const {SlashCommandBuilder} = require('@discordjs/builders');
-const {REST} = require('@discordjs/rest');
-const {Routes} = require('discord-api-types/v9');
-const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const { ContextMenuCommandBuilder, ApplicationCommandType, EmbedBuilder, SlashCommandBuilder, REST, Routes } = require('discord.js');
 const { scheduleJob } = require('node-schedule');
+const { Moon } =require('lunarphase-js');
 
 module.exports = async (Client) => {
     Client.log.info('WebSocket connection to Discord has been established');
 
     Client.log.info('Starting commands publication');
 
-    function closeChannels() {
-        console.log('CLOSING CHANNELS')
+    Client.Client.closeChannels = async () => {
         for (let role of Object.keys(Client.settings.toClose.roles)) {
-            console.log(role)
             let guild = Client.guilds.cache.get(process.env.MAIN_GUILD_ID);
             if (guild) {
                 let discordRole = guild.roles.cache.get(role);
                 if (discordRole) {
-                    console.log(Client.settings.toClose.roles[role])
                     for (let channelID of Client.settings.toClose.roles[role]) {
                         let channel = guild.channels.cache.get(channelID);
                         if (channel) {
                             switch (channel.type) {
-                                case 'GuildText':
+                                case 0:
                                     channel.permissionOverwrites.edit(role, {
                                         SendMessages: false,
                                         AddReactions: false,
-                                        SendMessagesInThreads: false
+                                        SendMessagesInThreads: false,
+                                        AddReactions: false
                                     });
                                     break;
 
-                                case 'GuildVoice':
+                                case 2:
                                     channel.permissionOverwrites.edit(role, {
                                         Speak: false,
-                                        Stream: false,
-                                        Join: false
+                                        SendMessages: false,
+                                        Connect: false
+                                    });
+                                    channel.members.forEach(member => {
+                                        member.voice.disconnect();
                                     });
                                     break;
+                            }
+                            
+                            if (channel.id == Client.settings.mainChannelID) {
+                                let message = await channel.send({
+                                    embeds: [
+                                        new EmbedBuilder()
+                                            .setTitle('<:off:895691615593705512> Fermeture des salons textuels et vocaux.')
+                                            .setDescription('✨ *L\'équipe vous souhaite une très belle nuit, à demain...*\n\n:last_quarter_moon_with_face: __Pourquoi ferme-t-on les salons la nuit ?__\n\n*En tant qu\'association reconnue d\'action sociale, nous avons la responsabilité de ce qui se passe sur notre discord. Pour permettre le repos de nos équipes ainsi que préserver le sommeil de tous, nous fermons les salons vocaux et textuels la nuit.*\n\nPS: Si cette nuit tu ne vas pas bien n\'hésites pas à te rendre ici : <#718250345951658064>')
+                                            .setImage('https://cdn.discordapp.com/attachments/847519151890366514/1297859871609196564/Capture_decran_2024-10-21_a_11.51.16.png?ex=677315f2&is=6771c472&hm=c719ae442af54b06745339f68f0a45b6e6e6ea99b68a942a5d21a4da25c67a9f&')
+                                            .setColor('204051')
+                                    ]
+                                });
+                                
+                                let guildEmoji = await Client.guilds.fetch(Client.settings.toClose.emojisGuildID);
+                                if (guildEmoji) {
+                                    for (let i in Client.settings.toClose.emojis) {
+                                        let emoji = await guildEmoji.emojis.fetch(Client.settings.toClose.emojis[i]);
+                                        if (emoji) {
+                                            await message.react(emoji);
+                                        }
+                                    }
+                                    message.react(Moon.lunarPhaseEmoji());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Client.Client.openChannels = async () => {
+        for (let role of Object.keys(Client.settings.toClose.roles)) {
+            let guild = Client.guilds.cache.get(process.env.MAIN_GUILD_ID);
+            if (guild) {
+                let discordRole = guild.roles.cache.get(role);
+                if (discordRole) {
+                    for (let channelID of Client.settings.toClose.roles[role]) {
+                        let channel = guild.channels.cache.get(channelID);
+                        if (channel) {
+                            switch (channel.type) {
+                                case 0:
+                                    channel.permissionOverwrites.edit(role, {
+                                        SendMessages: true,
+                                        AddReactions: true,
+                                        SendMessagesInThreads: true,
+                                        AddReactions: true
+                                    });
+                                    break;
+
+                                case 2:
+                                    channel.permissionOverwrites.edit(role, {
+                                        Speak: true,
+                                        SendMessages: true,
+                                        Connect: true
+                                    });
+                                    break;
+                            }
+                            if (channel.id == Client.settings.mainChannelID) {
+                                let message = await channel.send({
+                                    embeds: [
+                                        new EmbedBuilder()
+                                            .setTitle('<:onn:895691557817180191> Bonjour à toutes et à tous, les canaux vocaux et textuels sont ouverts.')
+                                            .setDescription('☀️ *Nous comptons sur vous pour avoir des échanges et des propos corrects.*')
+                                            .setImage('https://cdn.discordapp.com/attachments/847519151890366514/1297634244733964288/BONJOUR-2.png?ex=67739550&is=677243d0&hm=5422be8cc7705b65887d79d0d6345eab2bfe5394c872bd525e8abb9e5f1cc952&')
+                                            .setColor('9bd2d2')
+                                    ]
+                                });
+
+                                message.react('👋');
                             }
                         }
                     }
@@ -51,37 +120,38 @@ module.exports = async (Client) => {
             let closeTime = Client.settings.toClose.schedule[weekDay][1] || Client.settings.toClose.schedule.default[1];
             switch (weekDay) {
                 case 'monday':
-                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 1`, closeChannels());
-                    console.log('Scheduled close for monday')
+                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 1`, () => Client.openChannels());
+                    scheduleJob(`${closeTime.split('h')[1]} ${closeTime.split('h')[0]} * * 1`, () => Client.closeChannels());
                     break;
 
                 case 'tuesday':
-                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 2`, closeChannels());
-                    console.log('Scheduled close for tuesday')
+                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 2`, () => Client.openChannels());
+                    scheduleJob(`${closeTime.split('h')[1]} ${closeTime.split('h')[0]} * * 2`, () => Client.closeChannels());
                     break;
 
                 case 'wednesday':
-                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 3`, closeChannels());
-                    console.log('Scheduled close for wednesday')
+                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 3`, () => Client.openChannels());
+                    scheduleJob(`${closeTime.split('h')[1]} ${closeTime.split('h')[0]} * * 3`, () => Client.closeChannels());
                     break;
 
                 case 'thursday':
-                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 4`, closeChannels());
-                    console.log('Scheduled close for thursday')
+                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 4`, () => Client.openChannels());
+                    scheduleJob(`${closeTime.split('h')[1]} ${closeTime.split('h')[0]} * * 4`, () => Client.closeChannels());
                     break;
 
                 case 'friday':
-                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 5`, closeChannels());
-                    console.log('Scheduled close for friday')
+                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 5`, () => Client.openChannels());
+                    scheduleJob(`${closeTime.split('h')[1]} ${closeTime.split('h')[0]} * * 5`, () => Client.closeChannels());
                     break;
                 
                 case 'saturday':
-                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 6`, closeChannels());
+                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 6`, () => Client.openChannels());
+                    scheduleJob(`${closeTime.split('h')[1]} ${closeTime.split('h')[0]} * * 6`, () => Client.closeChannels());
                     break;
 
                 case 'sunday':
-                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 0`, closeChannels());
-                    console.log('Scheduled close for sun')
+                    scheduleJob(`${openTime.split('h')[1]} ${openTime.split('h')[0]} * * 0`, () => Client.openChannels());
+                    scheduleJob(`${closeTime.split('h')[1]} ${closeTime.split('h')[0]} * * 0`, () => Client.closeChannels());
                     break;
             }
         }
